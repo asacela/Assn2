f6769907b62fdfd4cc7f3d7b6939cbfb89787f63
Justin Gomez <justingomez@JDG-MacBook-Pro-8.local>
2019-10-17 11:20:40 -0700

    Created skeleton of UI for testing in main

--- main.cpp
+++ main.cpp
@@ -5,6 +5,29 @@ using namespace std;
 
 int main() {
 	Stack list;
+	char userChar = ' ';
+	while (userChar != 'q'){
+		cout << "Enter a for pushFirst" << endl
+				 << "Enter b for pushSecond" << endl
+				 << "Enter c for popFirst" << endl
+				 << "Enter d for popSecond" << endl
+				 << "Enter e for peekFirst" << endl
+				 << "Enter f for peekSecond" << endl
+				 << "Enter p to print" << endl
+				 << "Enter q to quit" << endl;
+		cin >> userChar;
+
+		if(userChar == 'a'){}
+		else if(userChar == 'b'){}
+		else if(userChar == 'c'){}
+		else if(userChar == 'd'){}
+		else if(userChar == 'e'){}
+		else if(userChar == 'f'){}
+		else if(userChar == 'p'){}
+		else{
+			cout << "Invalid command" << endl;
+		}
+	}
 
 	return 0;
 }
